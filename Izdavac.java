a616ad481bcb28ae91d49b63b2a193dfd8b655cf
jazavac <ante.sekulic.seky@gmail.com>
2015-02-10 21:17:15 +0100

    Create Izdavac.java

---
+++ Izdavac.java
@@ -0,0 +1,21 @@
+package hr.tvz.java.vjezbe.entitet;
+
+public class Izdavac {
+	
+	private String nazivIzdavaca;
+	private String drzavaIzdavaca;
+	
+	public Izdavac(String nazivIzdavaca, String drzavaIzdavaca) {
+		this.nazivIzdavaca = nazivIzdavaca;
+		this.drzavaIzdavaca = drzavaIzdavaca;
+	}
+	
+	public String getNazivIzdavaca() {
+		return nazivIzdavaca;
+	}
+	
+	public String getDrzavaIzdavaca() {
+		return drzavaIzdavaca;
+	}
+		
+}
