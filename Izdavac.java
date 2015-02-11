3751c0e07102837627ce168a62dbd07dc1d0115b
jazavac <ante.sekulic.seky@gmail.com>
2015-02-11 01:34:21 +0100

    Update Izdavac.java

--- Izdavac.java
+++ Izdavac.java
@@ -17,5 +17,38 @@ public class Izdavac {
 	public String getDrzavaIzdavaca() {
 		return drzavaIzdavaca;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((drzavaIzdavaca == null) ? 0 : drzavaIzdavaca.hashCode());
+		result = prime * result
+				+ ((nazivIzdavaca == null) ? 0 : nazivIzdavaca.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Izdavac))
+			return false;
+		Izdavac other = (Izdavac) obj;
+		if (drzavaIzdavaca == null) {
+			if (other.drzavaIzdavaca != null)
+				return false;
+		} else if (!drzavaIzdavaca.equals(other.drzavaIzdavaca))
+			return false;
+		if (nazivIzdavaca == null) {
+			if (other.nazivIzdavaca != null)
+				return false;
+		} else if (!nazivIzdavaca.equals(other.nazivIzdavaca))
+			return false;
+		return true;
+	}
 		
 }
